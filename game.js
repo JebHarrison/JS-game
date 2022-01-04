b5066f200be879c463fb5822569f347376b8b04d
James Harrison <jamesebharrison@hotmail.com>
2022-01-04 14:19:39 +0000

    importing and exporting snake file to game file for sync

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import { SNAKE_SPEED } from './snake'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake'
 
 let lastRenderTime = 0 
 
@@ -22,9 +22,9 @@ function main(currentTime) {
 window.requestAnimationFrame(main)
 
 function update(){
-
+    update()
 }
 
 function draw(){
-
+    drawSnake()
 }
--- snake.js
+++ snake.js
@@ -1,1 +1,9 @@
 export const SNAKE_SPEED = 1
+
+export function update(){
+    console.log('update snake')
+}
+
+export function draw(){
+    console.log('draw snake')
+}
