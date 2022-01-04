2ec47473a1e47a04717d56edbe1df762a5c70e2b
James Harrison <jamesebharrison@hotmail.com>
2022-01-04 15:21:45 +0000

    added gameBoard.innerhtml to remove trailing boxes

--- snake.js
+++ snake.js
@@ -1,12 +1,16 @@
 export const SNAKE_SPEED = 1
-const snakebody = [{x: 11, y:11}]
+const snakeBody = [{x: 11, y:11}]
 
 export function update(){
-    console.log('update snake')
+    for (let i =snakeBody.length - 2; i >=0; i--){
+    snakeBody[i +1] = { ...snakeBody[i]}
+    }
+    snakeBody[0].x += 1
+    snakeBody[0].y +=0
 }
 
 export function draw(gameBoard){
-    snakebody.forEach(segmant => {
+    snakeBody.forEach(segmant => {
         const snakeElement = document.createElement('div')
         snakeElement.style.gridRowStart = segmant.x
         snakeElement.style.gridColumnStart = segmant.y
