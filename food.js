f56e96efaf8691b48582244082740469a48d6a05
James Harrison <jamesebharrison@hotmail.com>
2022-01-05 08:41:34 +0000

    imported food in game file to update

--- food.js
+++ food.js
@@ -6,9 +6,9 @@ export function update(){
 
 export function draw(gameBoard) {
     const foodElement = document.createElement('div')
-    foodElement.style.gridRowStart = food.x
-    foodElement.style.gridColumnStart = food.y
-    foodElement.classList.add('snake')
+    foodElement.style.gridRowStart = food.y
+    foodElement.style.gridColumnStart = food.x
+    foodElement.classList.add('food')
     gameBoard.appendChild(foodElement)
 
 }
