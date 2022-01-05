9a60cfed3ab514949ff80f82b9d6d7b148f57c76
James Harrison <jamesebharrison@hotmail.com>
2022-01-05 09:24:59 +0000

    added let newSegments so previous code runs

--- snake.js
+++ snake.js
@@ -2,9 +2,11 @@ import { getInputDirection } from "./input.js"
 
 export const SNAKE_SPEED = 5
 const snakeBody = [{x: 11, y:11}]
+let newSegmants = 0
 
 export function update(){
     addSegments()
+    
     const inputDirection = getInputDirection()
     for (let i =snakeBody.length - 2; i >=0; i--){
     snakeBody[i +1] = { ...snakeBody[i]}
