6385035df948dfc4e479f280daf6b0fa7b5547ea
James Harrison <jamesebharrison@hotmail.com>
2022-01-06 13:33:24 +0000

    added to confirm message

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
  if (gameOver) {
-     if (confirm('You lost. Press ok to restart.')) {
+     if (confirm('Oh dear, You lost. Press ok to restart.')) {
          window.location = '/'
      }
      return
