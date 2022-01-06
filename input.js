2f3a276bc01886dc3ae4e08ca7fe099b0f21336f
James Harrison <jamesebharrison@hotmail.com>
2022-01-06 11:42:14 +0000

    changed last input direction in if statement to match axis

--- input.js
+++ input.js
@@ -4,24 +4,23 @@ let lastInputDirection = { x: 0, y: 0}
 window.addEventListener('keydown', e => {
     switch (e.key) {
         case 'ArrowUp':
-            if (lastInputDirection.x !== 0) break
-         inputDirection = { x: -1, y: 0}
-         break
-        case 'ArrowDown':
-            if (lastInputDirection.x !== 0) break
-         inputDirection = { x: 1, y: 0}
-         break
-        case 'ArrowLeft':
             if (lastInputDirection.y !== 0) break
          inputDirection = { x: 0, y: -1}
          break
-        case 'ArrowRight':
+        case 'ArrowDown':
             if (lastInputDirection.y !== 0) break
-            inputDirection = { x: 0, y: 1}
+         inputDirection = { x: 0, y: 1}
+         break
+        case 'ArrowLeft':
+            if (lastInputDirection.x !== 0) break
+         inputDirection = { x: -1, y: 0}
+         break
+        case 'ArrowRight':
+            if (lastInputDirection.x !== 0) break
+            inputDirection = { x: 1, y: 0}
             break
         
         }
-        console.log(lastInputDirection)
 })
 
 export function getInputDirection(){
